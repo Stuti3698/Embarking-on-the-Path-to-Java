9b69bd5bad431a03ee329e6246beb498b2ce2c26
Stuti Goel <132576375+Stuti3698@users.noreply.github.com>
2023-09-20 14:36:59 +0530

    Create Exercise4.java

---
+++ Exercise4.java
@@ -0,0 +1,16 @@
+//To illustrate the use of Binary Literals
+package exercise4;
+
+public class exercise4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b=0b00111001;
+		short s=0b001100100;
+		System.out.println("The binary number of byte is "+b);
+		System.out.println("The binary number of short is "+s);
+		
+
+	}
+
+}
