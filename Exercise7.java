5a6b26960bd2580be249bcd81c0cfc8d334d1513
Stuti Goel <132576375+Stuti3698@users.noreply.github.com>
2023-09-20 14:42:26 +0530

    Create Exercise7.java

---
+++ Exercise7.java
@@ -0,0 +1,27 @@
+//Program to perform relational operators
+package exercise7;
+
+import java.util.Scanner;
+
+public class exercise7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("a==b is"+(a==b));
+		System.out.println("a!=b is"+(a!=b));
+		System.out.println("a<b is"+(a<b));
+		System.out.println("a>b is"+(a>b));
+		System.out.println("a<=b is"+(a<=b));
+		System.out.println("a>=b is"+(a>=b));
+		//System.out.println("a==b is"+(a==b));
+		//System.out.println("a==b is"+(a==b));
+		
+		sc.close();
+		
+
+	}
+
+}
