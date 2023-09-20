f431439ce70a6c5b9645f94f95644210571ec579
Stuti Goel <132576375+Stuti3698@users.noreply.github.com>
2023-09-20 14:39:18 +0530

    Create Exercise5.java

---
+++ Exercise5.java
@@ -0,0 +1,23 @@
+//Java program to yse underscore in numeric literal
+package exercise5;
+
+public class exercise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int a1=_52;
+		int a2=5_2;
+		//int a3=52_;
+		System.out.println("a1 is "+a2);
+		int a3=0_52;
+		System.out.println("a3 value is"+a3);
+		int a4=3.1_44472;
+		System.out.println("a4 value is"+a4);
+		int a5=99_009L;
+		System.out.println("a5 value is "+a5);
+		
+		
+
+	}
+
+}
