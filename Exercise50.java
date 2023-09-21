5a9cb9f5c31af58b23ea1b23652c19a652ec5381
Stuti Goel <132576375+Stuti3698@users.noreply.github.com>
2023-09-21 12:47:52 +0530

    Create Exercise50.java

---
+++ Exercise50.java
@@ -0,0 +1,14 @@
+//  To print 1 to 10 using for loop
+package exercise50;
+
+public class exercise50 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
